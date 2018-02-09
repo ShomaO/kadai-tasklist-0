84d5e35d3dfd8a11304b56d24b526d42e7087b41
shoma otuka <ootuka@tohkon.co.jp>
2018-02-09 06:37:31 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,39 @@
+class TasksController < ApplicationController
+  
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'Task が正常に登録されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task が登録されませんでした'
+      render :new
+    end
+    
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+  
+  
+end
